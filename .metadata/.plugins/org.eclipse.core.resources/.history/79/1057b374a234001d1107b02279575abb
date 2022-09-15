b1507b2e9381982f42511272b7240a09073ad611
DannyHaarmann <94137078+DannyHaarmann@users.noreply.github.com>
2022-09-14 22:17:23 -0500

    Test for visible code

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/79/1057b374a234001d1107b02279575abb
@@ -0,0 +1,48 @@
+
+
+	
+
+
+
+public class Driver 
+{
+	public static void main(String[] args)
+	{
+		
+		int arr[] = { 3,65,6565,234,78,6,1022};
+	
+		
+		
+
+		
+		
+		bubbleSort(arr);
+		System.out.println("The top sales number are: ");
+		for(int i = 0; i < 5;i++)
+		{
+			System.out.println(arr[i] + " ");
+		}
+	}
+	public static void bubbleSort( int[] arr) 
+	 {  
+	        int n = arr.length;  
+	        int temp = 0;  
+	         for(int i=0; i < n; i++)
+	         {  
+	                 for(int j=1; j < (n-i); j++)
+	                 {  
+	                          if(arr[j-1] < arr[j])
+	                          {  
+	                                 
+	                                 temp = arr[j-1];  
+	                                 arr[j-1] = arr[j];  
+	                                 arr[j] = temp;  
+	                         }  
+	                          
+	                 }  
+	         }  
+	 }	
+	
+
+		
+}
--- Salesmen task/Driver.java
+++ Salesmen task/Driver.java
@@ -1,48 +1,35 @@
-
-
-	
-
-
+import java.util.ArrayList;
 
 public class Driver 
 {
+	ArrayList<String> bookshelf = new ArrayList<String>();
 	public static void main(String[] args)
 	{
-		
-		int arr[] = { 3,65,6565,234,78,6,1022};
-	
-		
-		
-
-		
-		
-		bubbleSort(arr);
-		System.out.println("The top sales number are: ");
-		for(int i = 0; i < 5;i++)
+	Salesman sales[] = new Salesman[10];
+	sales[1]= new Salesman("jane" , 101312);
+	sales[2]= new Salesman("gwen" , 53753);
+	sales[3]= new Salesman("chris" , 12);
+	sales[4]= new Salesman("holly" , 2212);
+	sales[6]= new Salesman("Kyle" , 999);
+	sales[7]= new Salesman("hunter" , 882);
+	sales[8]= new Salesman("elizebeth" , 11112);
+	sales[9]= new Salesman("joe" , 101255);
+	sales[0]= new Salesman("bob" , 10);
+	Salesman result[] = Salesman.getTopFive(sales);
+		for(int i = 0; i < 10; i++)
 		{
-			System.out.println(arr[i] + " ");
+		System.out.println(sales[i]);
+		
+		}
+		System.out.println("The top five salesmen are: ");
+		for(int i = 0; i < 5; i++)
+		{
+		System.out.println(result[i]);
 		}
 	}
-	public static void bubbleSort( int[] arr) 
-	 {  
-	        int n = arr.length;  
-	        int temp = 0;  
-	         for(int i=0; i < n; i++)
-	         {  
-	                 for(int j=1; j < (n-i); j++)
-	                 {  
-	                          if(arr[j-1] < arr[j])
-	                          {  
-	                                 
-	                                 temp = arr[j-1];  
-	                                 arr[j-1] = arr[j];  
-	                                 arr[j] = temp;  
-	                         }  
-	                          
-	                 }  
-	         }  
-	 }	
+	
+	
+	
 	
 
-		
 }
